7ee167210be894ce42c7a79733f532a614fcf818
Alfonso <alfcrisci@gmail.com>
2015-11-20 15:13:21 +0100

    Update channel_analytic.r

--- R/channel_analytic.r
+++ R/channel_analytic.r
@@ -184,8 +184,8 @@ channel_analytic=function(channel_obj,use_channel_dates=TRUE, start_date=NULL, e
     channel_obj$data=as.Date(channel_obj$created)
     channel_obj$hour=lubridate::hour(channel_obj$created)
     channel_obj$month=lubridate::month(channel_obj$created)
-    channel_obj$text=iconv(all_tweets$text,"utf-8")
-    channel_obj$isRetweet=as.integer(all_tweets$isRetweet)
+    channel_obj$text=iconv(channel_obj$text,"utf-8")
+    channel_obj$isRetweet=as.integer(channel_obj$isRetweet)
     channel_obj$screeName=channel_obj$screenName
     channel_obj$screenName<-NULL
   }
