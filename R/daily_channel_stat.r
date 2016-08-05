a0558c6023d4b5af4257364e6b23a718e6537adb
Alfonso <alfcrisci@gmail.com>
2016-08-05 14:24:09 +0200

    Update daily_channel_stat.r

--- R/daily_channel_stat.r
+++ R/daily_channel_stat.r
@@ -14,21 +14,22 @@
 #'
 #'
 #'
-daily_channel_stat=function(x,stream="DISIT") 
+daily_channel_stat=function(x,stream="") 
   { res_d=list();
     datetemp=unique(x$date);
 
     for ( i in 1:length(datetemp)) {    
           temp=x[which(x$date == datetemp[i]),]
-          temp_df=fastChannelstat(temp,check_duplicates=T,stream=stream)
-          res_d[[i]]=suppressWarnings(data.frame(date=datetemp[i],temp_df))
+          res_d[[i]]=fastChannelstat(temp,check_duplicates=T)
   
           }
 
     res_daily=do.call("rbind",res_d)
+    
+    if ( nrow(res_daily) == 1) {  return(res_daily)
+                               }
+    
     date_full=data.frame(date=seq(range(res_daily$date)[1],range(res_daily$date)[2],by=1))
     daily_channel_final=merge(date_full,res_daily,all.x=T)
-    daily_channel_final$ini_date_full=NULL
-    daily_channel_final$end_date_full=NULL
     return(daily_channel_final)
 }
