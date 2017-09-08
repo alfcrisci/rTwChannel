8e22e4451402e85e9005af8aef76dc2fdfa5abaf
Alfonso <alfcrisci@gmail.com>
2017-09-08 08:04:06 +0200

    Update getCorpus.r

--- R/getCorpus.r
+++ R/getCorpus.r
@@ -3,8 +3,9 @@
 #' @description Creating corpus object by using tm R package a from messages.
 #'
 #' @param  textVector   Character  Message of tweet
-#' @param  hashtag    remove hashtag from corpus
-#' @return  Return   the tm corpus of message purged by mentions and links.
+#' @param  hashtag    logical remove hashtag from corpus
+#' @param  elim_words    character the words to be removed from corpus
+#' @return  Return   a tm corpus of message purged by mentions and links.
 #' @author  Istituto di Biometeorologia Firenze Italy  Alfonso crisci \email{a.crisci@@ibimet.cnr.it}
 #' @keywords  Tm, corpus
 #'
@@ -24,6 +25,6 @@ getCorpus <-function(textVector, hashtag=FALSE,elim_words)
   doc.corpus <- tm::tm_map(doc.corpus, removeNumbers)
   doc.corpus <- tm::tm_map(doc.corpus, removePunctuation)
   doc.corpus <- tm::tm_map(doc.corpus, PlainTextDocument)
-   doc.corpus <- tm_map(text_corpus, removeWords, elim_words)
+   doc.corpus <- tm_map(doc.corpus, removeWords, elim_words)
   return(doc.corpus)
 }
